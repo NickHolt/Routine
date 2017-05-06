7bdf90acd9b83cd9ffdbbcc8195332e3fd8a0bfc
Nick Holt <nickholt@driver.xyz>
2017-05-06 16:03:59 -0700

    Create activity detail view

---
+++ Routine/ActivityDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ActivityDetailViewController.swift
+//  Routine
+//
+//  Created by Nick Holt on 5/6/17.
+//  Copyright © 2017 Redox. All rights reserved.
+//
+
+import UIKit
+
+class ActivityDetailViewController: UIViewController {
+    var activity: Activity!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Populate activity data
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Save activity data
+    }
+}
