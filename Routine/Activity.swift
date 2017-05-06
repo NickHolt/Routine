35da69e090a3bebcda5f7b847c53633c379b6da0
Nick Holt <nickholt@driver.xyz>
2017-05-06 12:05:55 -0700

    Use enum type for days of week

--- Routine/Activity.swift
+++ Routine/Activity.swift
@@ -8,11 +8,21 @@
 
 import Foundation
 
+enum DayOfWeek: UInt8 {
+    case Monday    = 1
+    case Tuesday   = 2
+    case Wednesday = 4
+    case Thursday  = 8
+    case Friday    = 16
+    case Saturday  = 32
+    case Sunday    = 64
+}
+
 class Activity: NSObject {
     var name: String
-    var daysOfWeek: UInt8
+    var daysOfWeek: [DayOfWeek]
     
-    init(name: String, daysOfWeek: UInt8) {
+    init(name: String, daysOfWeek: [DayOfWeek]) {
         self.name = name
         self.daysOfWeek = daysOfWeek
     }
