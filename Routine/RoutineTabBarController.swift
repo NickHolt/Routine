7dae3b9879b025102a4935dfb4829faeb5850dcb
Nick Holt <nickholt@driver.xyz>
2017-05-06 11:35:56 -0700

    Create tab bar controller for global control

--- Routine/RoutineTabBarController.swift
+++ Routine/RoutineTabBarController.swift
@@ -1,25 +1,23 @@
 //
-//  ViewController.swift
+//  RoutineTabBarController.swift
 //  Routine
 //
-//  Created by Nick Holt on 5/3/17.
+//  Created by Nick Holt on 5/6/17.
 //  Copyright © 2017 Redox. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class RoutineTabBarController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
