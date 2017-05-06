ae2831af0b2574c33881d866a2c5f9373efbfcce
Nick Holt <nickholt@driver.xyz>
2017-05-06 11:51:42 -0700

    Create new view controller for Daily Activities view

---
+++ Routine/DailyActivitiesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DailyActivitiesViewController.swift
+//  Routine
+//
+//  Created by Nick Holt on 5/6/17.
+//  Copyright © 2017 Redox. All rights reserved.
+//
+
+import UIKit
+
+class DailyActivitiesViewController: UITableViewController {
+    
+}
