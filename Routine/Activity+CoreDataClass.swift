245ccb43ec08d3229a0432e4911c4e08da96f9ad
Nick Holt <nickholt@driver.xyz>
2017-05-13 12:49:52 -0700

    Automatically set and populate data from the daysOfWeekBitField

--- Routine/Activity+CoreDataClass.swift
+++ Routine/Activity+CoreDataClass.swift
@@ -11,5 +11,34 @@ import CoreData
 
 @objc(Activity)
 public class Activity: NSManagedObject {
-    var daysOfWeek: [DayOfWeek]?
+    var daysOfWeek: [DayOfWeek]! {
+        didSet {
+            // Set daysOfWeekBitField
+            daysOfWeekBitField = 0
+            
+            guard let days = daysOfWeek else {
+                return
+            }
+
+            for day in days {
+                daysOfWeekBitField |= Int32(1 << day.rawValue)
+            }
+            
+            print("Set daysOfWeekBitField for \(title ?? "UNKNOWN"): \(String(daysOfWeekBitField, radix:2))")
+        }
+    }
+    
+    override public func awakeFromFetch() {
+        // Set daysOfWeek
+        var daysOfWeek: [DayOfWeek] = []
+
+        for i in DayOfWeek.Monday.rawValue...DayOfWeek.Sunday.rawValue {
+            if daysOfWeekBitField & Int32(1 << i) != 0 {
+                daysOfWeek.append(DayOfWeek(rawValue: i)!)
+            }
+        }
+        
+        self.daysOfWeek = daysOfWeek
+        print("Set daysOfWeek for \(title ?? "UNKNOWN"): \(daysOfWeek)")
+    }
 }
