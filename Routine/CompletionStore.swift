ab4bf6755149e8c94d2aac4112c36d0ee7fa3bd7
Nick Holt <nickholt@driver.xyz>
2017-05-14 15:48:10 -0700

    Implement registering new completions

--- Routine/CompletionStore.swift
+++ Routine/CompletionStore.swift
@@ -12,24 +12,56 @@ class CompletionStore: EntityStore {
     
     var allCompletions = [Completion]()
     
-    func registerCompletion(for activity: Activity, on date: Date) {
+    
+    func getCompletions(for activity: Activity) -> [Completion] {
         // Stub
     }
     
-    func registerNonCompletion(for activity: Activity, on date: Date) {
+    func getCompletions(for date: Date) -> [Completion] {
         // Stub
     }
     
-    func getCompletions(for activity: Activity) {
+    func getCompletions(for activity: Activity, on date: Date) -> [Completion] {
         // Stub
     }
     
-    func getCompletions(for date: Date) {
-        // Stub
+    @discardableResult
+    private func registerCompletion(for activity: Activity, on date: Date, wasCompleted: Bool) -> Completion {
+        
+        deleteCompletions(for: activity, on: date)
+
+        let context = persistentContainer.viewContext
+        var completion: Completion!
+        
+        context.performAndWait {
+            completion = Completion(context: context)
+            completion.activity = activity
+            completion.date = date
+            completion.wasCompleted = wasCompleted
+        }
+        
+        allCompletions.append(completion)
+        
+        return completion
     }
     
-    func getCompletions(for activity: Activity, on date: Date) {
-        // Stub
+    @discardableResult func registerCompletion(for activity: Activity, on date: Date) -> Completion {
+        return registerCompletion(for: activity, on: date, wasCompleted: true)
+    }
+    
+    @discardableResult func registerNonCompletion(for activity: Activity, on date: Date) -> Completion {
+        return registerCompletion(for: activity, on: date, wasCompleted: false)
+    }
+    
+    private func deleteCompletions(for activity: Activity, on date: Date) {
+        let completions = getCompletions(for: activity, on: date)
+        assert(completions.count == 1, "Multiple completions found for \(activity) on \(date)")
+        
+        let context = persistentContainer.viewContext
+        
+        for completion in completions {
+            context.delete(completion)
+        }
     }
     
     func loadFromDisk() throws {
