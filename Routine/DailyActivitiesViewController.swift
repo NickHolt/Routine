c0e74257ad2b4fc69f43aa637cad6febecc85fa8
Nick Holt <nickholt@driver.xyz>
2017-05-30 19:44:49 -0700

    Create cell type for adding Activity occurrence

--- Routine/DailyActivitiesViewController.swift
+++ Routine/DailyActivitiesViewController.swift
@@ -215,7 +215,7 @@ class DailyActivitiesViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+                
         configureEdgePanGestures()
         
         configureTapGesture()
@@ -235,13 +235,13 @@ class DailyActivitiesViewController: UITableViewController {
         switch status {
         case .completed:
             cell.accessoryType = .checkmark
-            cell.backgroundColor = nil
+            cell.backgroundColor = .white
         case .excused:
             cell.accessoryType = .none
             cell.backgroundColor = .lightGray
         case .notCompleted:
             cell.accessoryType = .none
-            cell.backgroundColor = nil
+            cell.backgroundColor = .white
         }
     }
     
@@ -295,15 +295,52 @@ class DailyActivitiesViewController: UITableViewController {
 // MARK: UITableViewController methods
 extension DailyActivitiesViewController {
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard section == 0 else {
+            return nil
+        }
+        
+        return "Recurring Activities"
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            return 65
+        case 1:
+            return 32
+        default:
+            preconditionFailure("Unexpected section: \(indexPath.section)")
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+        
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return currentActivities.count
+        switch section {
+        case 0:
+            return currentActivities.count
+        case 1:
+            return 1
+        default:
+            preconditionFailure("Unexpected section: \(section)")
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "DailyActivitiesViewCell", for: indexPath)
+        switch indexPath.section {
+        case 0:
+            return tableView.dequeueReusableCell(withIdentifier: "DailyActivitiesViewCell", for: indexPath)
+        case 1:
+            return tableView.dequeueReusableCell(withIdentifier: "AddOccurrenceCell", for: indexPath)
+        default:
+            preconditionFailure("Unexpected section: \(indexPath.section)")
+        }
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    private func didSelectActivityRowAt(indexPath: IndexPath) {
         let activity = self.activity(for: indexPath)
         
         if completedActivities.contains(activity) {
@@ -317,7 +354,22 @@ extension DailyActivitiesViewController {
         configureTitle()
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.section {
+        case 0:
+            didSelectActivityRowAt(indexPath: indexPath)
+        case 1:
+            print("Add Activity occurrence")
+        default:
+            preconditionFailure("Unexpected section: \(indexPath.section)")
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard indexPath.section == 0 else {
+            return
+        }
+        
         let activity = self.activity(for: indexPath)
         
         let myCell = cell as! DailyActivitiesViewCell
@@ -333,15 +385,13 @@ extension DailyActivitiesViewController {
         }
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 65
-    }
-    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
+        return indexPath.section == 0
     }
     
     override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        precondition(indexPath.section == 0, "Editing should only be enabled for Activity section")
+        
         let activity = self.activity(for: indexPath)
         
         var archiveAction: UITableViewRowAction
