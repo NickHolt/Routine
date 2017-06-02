bc27977711db7feb5323c3350c4a77f5fe779228
Nick Holt <nickholt@driver.xyz>
2017-06-01 21:19:43 -0700

    Populate Activity selection view

--- Routine/ActivitySelectionViewController.swift
+++ Routine/ActivitySelectionViewController.swift
@@ -11,4 +11,32 @@ import os.log
 
 class ActivitySelectionViewController: UITableViewController {
     var activityStore: ActivityStore!
+    var activities: [Activity]!
+    
+    private func activity(_ firstActivity: Activity, shouldAppearBefore secondActivity: Activity) -> Bool {
+        guard let firstTitle = firstActivity.title?.lowercased() else {
+            return false
+        }
+        guard let secondTitle = secondActivity.title?.lowercased() else {
+            return true
+        }
+        
+        return firstTitle < secondTitle
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        activities = activityStore.getAllActiveActivities().sorted(by: activity(_:shouldAppearBefore:))
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return activities.count
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let activity = activities[indexPath.row]
+        
+        cell.textLabel?.text = activity.title
+    }
 }
