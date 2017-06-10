49a6c13eb1e16c87d5c01750e2ef4e0ebd6dfd04
Nick Holt <nickholt@driver.xyz>
2017-06-10 13:49:37 -0700

    Add tests for registering Completions

--- RoutineTests/CompletionHistoryTests.swift
+++ RoutineTests/CompletionHistoryTests.swift
@@ -17,6 +17,12 @@ class CompletionHistoryTests: RoutineTestCase {
     var today: Date {
         return Date()
     }
+    var yesterday: Date {
+        return Calendar.current.date(byAdding: .day, value: -1, to: today)!
+    }
+    var tomorrow: Date {
+        return Calendar.current.date(byAdding: .day, value: 1, to: today)!
+    }
     
     override func setUp() {
         super.setUp()
@@ -24,5 +30,82 @@ class CompletionHistoryTests: RoutineTestCase {
         completionHistory = CompletionHistory()
         completionHistory.activityStore = activityStore
         completionHistory.completionStore = completionStore
+    }
+    
+    func testRegisterSingleCompletionForSingleActivity() {
+        let activity = activityStore.getNewEntity()
+        
+        let completionStatus = Completion.Status.completed
+        
+        completionHistory.registerCompletion(for: activity, on: today, withStatus: completionStatus)
+        
+        let completion = completionHistory.getCompletion(for: activity, on: today)
+        
+        XCTAssertNotNil(completion, "Completion could not be retrieved")
+        XCTAssertEqual(completion?.status, completionStatus, "Completion status not properly set")
+    }
+    
+    func testRegisterMultipleCompletionsForSingleActivity() {
+        let activity = activityStore.getNewEntity()
+        
+        let expectedCompletionStatus = Completion.Status.excused
+        
+        completionHistory.registerCompletion(for: activity, on: today, withStatus: .completed)
+        completionHistory.registerCompletion(for: activity, on: today, withStatus: expectedCompletionStatus)
+        
+        let completion = completionHistory.getCompletion(for: activity, on: today)
+        
+        XCTAssertNotNil(completion, "Completion could not be retrieved")
+        XCTAssertEqual(completion!.status, expectedCompletionStatus, "Completion status not properly set")
+    }
+    
+    func testRegisterCompletionsForMultipleActivitiesOnDifferentDays() {
+        let activity0 = activityStore.getNewEntity()
+        let activity1 = activityStore.getNewEntity()
+
+        let completionStatus0 = Completion.Status.completed
+        let completionStatus1 = Completion.Status.excused
+        
+        completionHistory.registerCompletion(for: activity0, on: today, withStatus: completionStatus0)
+        completionHistory.registerCompletion(for: activity1, on: yesterday, withStatus: completionStatus1)
+        
+        let completion0 = completionHistory.getCompletion(for: activity0, on: today)
+        
+        XCTAssertNotNil(completion0, "Completion for first Activity could not be retrieved")
+        XCTAssertEqual(
+            completion0?.status,
+            completionStatus0,
+            "Completion status for first Activity not properly set"
+        )
+        XCTAssertNil(
+            completionHistory.getCompletion(for: activity0, on: yesterday),
+            "Completion was registered on incorrect day for first Activity"
+        )
+
+        let completion1 = completionHistory.getCompletion(for: activity1, on: yesterday)
+        
+        XCTAssertNotNil(completion1, "Completion for second Activity could not be retrieved")
+        XCTAssertEqual(
+            completion1?.status,
+            completionStatus1,
+            "Completion status for second Activity not properly set"
+        )
+        XCTAssertNil(
+            completionHistory.getCompletion(for: activity1, on: today),
+            "Completion was registered on incorrect day for second Activity"
+        )
+    }
+    
+    func testCanRegisterCompletionForActivityInFuture() {
+        let activity = activityStore.getNewEntity()
+        
+        let completionStatus = Completion.Status.completed
+        
+        completionHistory.registerCompletion(for: activity, on: tomorrow, withStatus: completionStatus)
+        
+        let completion = completionHistory.getCompletion(for: activity, on: tomorrow)
+        
+        XCTAssertNotEqual(completion, nil, "Completion could not be retrieved")
+        XCTAssertEqual(completion?.status, completionStatus, "Completion status not properly set")
     }
 }
